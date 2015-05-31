4859f11f94311ca92869a5c99746b859183fc8a2
blackendstudios <jeroflat@gmail.com>
2015-05-31 18:14:22 -0500

    basic static server

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 'use strict';
 // define http
 const http = require('http');
+const fs = require('fs');
 // with io.js we can obtain the environment variables of the operative system
 const port = process.env.PORT || 8080;
 // event emitter is node.js/io.js mechanism that lets you emit events and listen for those events
 // 'server' besides taking a callback, it's also an event emitter
 const server = http.createServer();
 //when is a 'request' is going to execute the function onRequest
-server.on('request', onRequest());
-server.on('listening', onListening());
+server.on('request', onRequest);
+server.on('listening', onListening);
 server.listen(port);
 
 function onRequest(req, res) {
-   res.end('hi ioj.s');
+   // instead of responding a 'message', respond with a file
+   // synchronous method
+   let file = fs.readFileSync('public/index.html');// readFileSync takes a path to a file and responds with that file
+   /*
+    * is not recommended using it synchronously because it'll block the main execution threat of io.js
+    * while it loads the all file from the system, when it finish loading the file, it'll block all of
+    * the request that's coming to the server and then sends the file.
+    */
+   res.end(file);
 }
 
 function onListening() {
