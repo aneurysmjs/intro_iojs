b339e980b0a0e1c8274cc96b89fa487793e7564b
blackendstudios <jeroflat@gmail.com>
2015-05-31 15:04:40 -0500

    extract callbacks to declared functions

--- server.js
+++ server.js
@@ -3,13 +3,21 @@
 const http = require('http');
 // with io.js we can obtain the environment variables of the operative system
 const port = process.env.PORT || 8080;
-// 'callbacks' are functions that are executed after an asynchronous method has finished executing
-// createServer() takes a function to be run whenever receives an http request
-const server = http.createServer(function(req, res) {
-   res.end('hi ioj.s');
-});
+/*
+ if we want maintainable code, the recommendation is that all of our callbacks(anonymous/asynchronous) functions
+ need to be defined/declared functions
+ */
+const server = http.createServer(onRequest);
 
-server.listen(port, function(){
+server.listen(port, onListening);
+
+function onRequest(req, res) {
+   res.end('hi ioj.s');
+}
+
+function onListening() {
    console.log('server listening on port ' + port);
-});
+}
+
+
 
