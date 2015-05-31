d675258d8eae1bc1c8410f67a5c6f5187fde7bda
blackendstudios <jeroflat@gmail.com>
2015-05-31 15:15:14 -0500

    adding 'event  emitters'

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const http = require('http');
 // with io.js we can obtain the environment variables of the operative system
 const port = process.env.PORT || 8080;
-/*
- if we want maintainable code, the recommendation is that all of our callbacks(anonymous/asynchronous) functions
- need to be defined/declared functions
- */
-const server = http.createServer(onRequest);
-
-server.listen(port, onListening);
+// event emitter is node.js/io.js mechanism that lets you emit events and listen for those events
+// 'server' besides taking a callback, it's also an event emitter
+const server = http.createServer();
+//when is a 'request' is going to execute the function onRequest
+server.on('request', onRequest());
+server.on('listening', onListening());
+server.listen(port);
 
 function onRequest(req, res) {
    res.end('hi ioj.s');
