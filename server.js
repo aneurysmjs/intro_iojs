f174da14a2c7535786ef26a3aff2311fa7e3c4f6
blackendstudios <jeroflat@gmail.com>
2015-05-31 14:52:54 -0500

    callbacks added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 'use strict';
-//define http
+// define http
 const http = require('http');
-//with io.js we can obtain the environment variables of the operative system
+// with io.js we can obtain the environment variables of the operative system
 const port = process.env.PORT || 8080;
-//create our http server
-const server = http.createServer();
+// 'callbacks' are functions that are executed after an asynchronous method has finished executing
+// createServer() takes a function to be run whenever receives an http request
+const server = http.createServer(function(req, res) {
+   res.end('hi ioj.s');
+});
+
+server.listen(port, function(){
+   console.log('server listening on port ' + port);
+});
+
