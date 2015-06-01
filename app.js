a26773f5ab3574ed04c722d3cbd1e9907bba28b4
blackendstudios <jeroflat@gmail.com>
2015-05-31 23:31:03 -0500

    basic routing to serve files depending on the URL

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log('hi app.js');
-// if we run the server, the page will load without problems.
-// but in the console with appear "SyntaxError: expected expression, got '<'"
-// if we look the source code of the page, it loaded everything in the correct order
-// if we click our app.js, it'll open the index.html
-// and that's because the main route of the server it serving just the index.html
-// no matter what we pass to the route, it'll always serve index.html
-// so we need to specify routes in order to see what kind of resources are loading
--- server.js
+++ server.js
@@ -10,11 +10,40 @@ server.on('listening', onListening);
 server.listen(port);
 
 function onRequest(req, res) {
+   let url = req.url;// get the URL
+   if(url.startsWith('/index') || url === '/') {
+      return serveIndex(res);
+   }
+   if(url === '/app.js') {
+      return serveApp(res);
+   }
+
+   res.statuCode = 404;
+   res.end(`Not found ${url}`)
+}
+
+function serveIndex(res) {
    let index = path.join(__dirname, 'public', 'index.html');
-   res.setHeader('Content-Type', 'text/html');
    let rs = fs.createReadStream(index);
+
+   res.setHeader('Content-Type', 'text/html');
    rs.pipe(res);
+
    rs.on('error', function(err) {
+      res.setHeader('Content-Type', 'text/html');
+      rs.end(err.message);
+   });
+}
+
+function serveApp(res) {
+   let app = path.join(__dirname, 'public', 'app.js');
+   let rs = fs.createReadStream(app);
+
+   res.setHeader('Content-Type', 'text/javascript');
+   rs.pipe(res);
+
+   rs.on('error', function(err) {
+      res.setHeader('Content-Type', 'text/html');
       rs.end(err.message);
    });
 }
