4fb7c5e5d8036570e8ccbc337d8c666ea0c76780
blackendstudios <jeroflat@gmail.com>
2015-05-31 22:13:42 -0500

    es6 string interpolation added

--- server.js
+++ server.js
@@ -11,21 +11,16 @@ server.listen(port);
 
 function onRequest(req, res) {
    let index = path.join(__dirname, 'public', 'index.html');
-   // streams are mechanism to read files or sources of binary information that reads in a smarter way.
-   // readFile loads the all file in memory, after the file is in memory, executes the callback and the file gets send in the response.
-   // so how it works when working with streams?
-   // while is reading the file it'll deliver as it goes having data available from the file and delivering it in the response.
    res.setHeader('Content-Type', 'text/html');
    let rs = fs.createReadStream(index);
-   // so is reading the file and sending it
-   rs.pipe(res);// here we're setting the
+   rs.pipe(res);
    rs.on('error', function(err) {
       rs.end(err.message);
    });
 }
 
 function onListening() {
-   console.log('server listening on port ' + port);
+   console.log(`server listening on port ${port}`);
 }
 
 
