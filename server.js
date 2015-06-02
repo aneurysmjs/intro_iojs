b68377e9ec42de62c07c7b5e9753051a5142b1c3
blackendstudios <jeroflat@gmail.com>
2015-06-01 22:39:58 -0500

    using 'st' for management of static files

--- server.js
+++ server.js
@@ -2,55 +2,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const router = require('./router');// it's serving 'index.html'
+
 const port = process.env.PORT || 8080;
 const server = http.createServer();
 
-server.on('request', onRequest);
+server.on('request', router);// instead a passing 'onRequest', we pass 'router' cuz it already has 'onRequest'
 server.on('listening', onListening);
 server.listen(port);
 
-function onRequest(req, res) {
-   let url = req.url;// get the URL
-   if(url.startsWith('/index') || url === '/') {
-      return serveIndex(res);
-   }
-   if(url === '/app.js') {
-      return serveApp(res);
-   }
-
-   res.statuCode = 404;
-   res.end(`Not found ${url}`)
-}
-
-function serveIndex(res) {
-   let index = path.join(__dirname, 'public', 'index.html');
-   let rs = fs.createReadStream(index);
-
-   res.setHeader('Content-Type', 'text/html');
-   rs.pipe(res);
-
-   rs.on('error', function(err) {
-      res.setHeader('Content-Type', 'text/html');
-      rs.end(err.message);
-   });
-}
-
-function serveApp(res) {
-   let app = path.join(__dirname, 'public', 'app.js');
-   let rs = fs.createReadStream(app);
-
-   res.setHeader('Content-Type', 'text/javascript');
-   rs.pipe(res);
-
-   rs.on('error', function(err) {
-      res.setHeader('Content-Type', 'text/html');
-      rs.end(err.message);
-   });
-}
+// now the server won't need serveIndex and serveApp because 'st' is doing the job for us
 
 function onListening() {
    console.log(`server listening on port ${port}`);
 }
-
-
 
